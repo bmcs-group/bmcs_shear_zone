0aee4e2f683167e5d5d3a91d07ac8fc672b032dd
rch <rostislav.chudoba@rwth-aachen.de>
2021-12-02 09:00:35 +0100

    renamed u_tija to U_tija

--- bmcs_shear/dic_crack/dic_grid.py
+++ bmcs_shear/dic_crack/dic_grid.py
@@ -7,7 +7,7 @@ import numpy as np
 
 class DICGrid(bu.Model):
 
-    name = 'DiC input'
+    name = 'DIC grid history'
     dir_name = bu.Str('<unnamed>', ALG=True)
 
     n_x = bu.Int(20, ALG=True)
@@ -22,6 +22,7 @@ class DICGrid(bu.Model):
     U_factor = bu.Float(100, ALG=True)
 
     t = bu.Float(1, ALG=True)
+
     def _t_changed(self):
         d_t = (1 / self.n_t)
         self.end_t = int( (self.n_t-1) * (self.t + d_t/2))
@@ -57,57 +58,45 @@ class DICGrid(bu.Model):
 
     grid_column_first = bu.Bool(True)
 
-    u_tija = tr.Property(depends_on='state_changed')
+    U_tija = tr.Property(depends_on='state_changed')
     '''Read the displacement data from the individual csv files'''
-    tr.cached_property
-    def _get_u_tija(self):
+    @tr.cached_property
+    def _get_U_tija(self):
         files = [join(self.data_dir, each)
                for each in sorted(os.listdir(self.data_dir))
                if each.endswith('.csv')]
-        u_tpa = np.array([
+        U_tpa = np.array([
             np.loadtxt(csv_file, dtype=float,
                        skiprows=1, delimiter=',', usecols=(2,3), unpack=False)
             for csv_file in files
         ], dtype=np.float_)
-        n_t, n_e, n_a = u_tpa.shape # get the dimensions of the time and entry dimensions
+        n_t, n_e, n_a = U_tpa.shape # get the dimensions of the time and entry dimensions
         n_x, n_y = self.n_x, self.n_y
         if self.grid_column_first:
-            u_tija = u_tpa.reshape(n_t, n_x, n_y, 2)  # for numbering from top right to bottom right
-            u_val = u_tija
-            #u_tjia = u_tpa.reshape(n_t, n_y, n_x, 2) # for numbering from bottom right to left
+            U_tija = U_tpa.reshape(n_t, n_x, n_y, 2)  # for numbering from top right to bottom right
         else:
-            u_tjia = u_tpa.reshape(n_t, n_y, n_x, 2) # for numbering from bottom right to left
-            u_tija = np.einsum('tjia->tija', u_tjia)
+            U_tjia = U_tpa.reshape(n_t, n_y, n_x, 2) # for numbering from bottom right to left
+            U_tija = np.einsum('tjia->tija', U_tjia)
         if self.grid_number_vertical:
-            return u_tija[:,:,::-1,:]
+            return U_tija[:,:,::-1,:]
         else:
-            return u_tija
+            return U_tija
 
     n_t = tr.Property(depends_on='state_changed')
     '''Read the displacement data from the individual csv files'''
     @tr.cached_property
     def _get_n_t(self):
-        return len(self.u_tija)
-
-
-    #grid_x_slice = tr.Any(slice(None,None,-1))
-    #grid_y_slice = tr.Any(slice(None,None,-1))
+        return len(self.U_tija)
 
     grid_number_vertical = bu.Bool(True)
 
     X_ija = tr.Property(depends_on='state_changed')
     '''Read the displacement data from the individual csv files'''
-    tr.cached_property
+    @tr.cached_property
     def _get_X_ija(self):
         n_x, n_y = self.n_x, self.n_y
-        #x_range = np.arange(n_x)[self.grid_x_slice] * self.d_x
         x_range = np.arange(n_x)[::-1] * self.d_x
-        #y_range = np.arange(n_y) * self.d_y #for beams having grid number from bottom right to left
-        # if self.grid_number_vertical:
-        #     y_range = np.arange(n_y)[::-1] * self.d_y #for beams having grid number from right top to bottom
-        # else:
-        y_range = np.arange(n_y) * self.d_y #for beams having grid number from bottom right to left
-        #y_range = np.arange(n_y)[self.grid_y_slice] * self.d_y
+        y_range = np.arange(n_y) * self.d_y
         y_ij, x_ij = np.meshgrid(y_range, x_range)
         X_aij = np.array([x_ij, y_ij])
         X_ija = np.einsum('aij->ija', X_aij)
@@ -118,10 +107,10 @@ class DICGrid(bu.Model):
     '''
     @tr.cached_property
     def _get_U_ija(self):
-        return self.u_tija[self.end_t] - self.u_tija[self.start_t]
+        return self.U_tija[self.end_t] - self.U_tija[self.start_t]
 
     def update_plot(self, axes):
         ax = axes
-        XU0_aij = np.einsum('ija->aij', self.X_ija + self.U_ija * self.U_factor)
-        ax.scatter(*XU0_aij.reshape(2,-1), s=15, marker='o', color='darkgray')
+        XU_aij = np.einsum('ija->aij', self.X_ija + self.U_ija * self.U_factor)
+        ax.scatter(*XU_aij.reshape(2,-1), s=15, marker='o', color='darkgray')
         ax.axis('equal');
