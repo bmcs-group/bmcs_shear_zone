fixed errors in dic_grid, corret rigid body rotation in aligned_grid

bmcs_shear/dic_crack/dic_grid.py
@@ -31,7 +31,6 @@ class DICGrid(bu.Model):
         bu.Item('n_y'),
         bu.Item('d_x'),
         bu.Item('d_y'),
-        bu.Item('start_t', readonly=True),
         bu.Item('end_t', readonly=True),
         bu.Item('U_factor'),
         time_editor=bu.HistoryEditor(
@@ -40,14 +39,17 @@ class DICGrid(bu.Model):
     )
 
     L_x = tr.Property
+    '''Width of the domain'''
     def _get_L_x(self):
         return self.d_x * (self.n_x-1)
 
     L_y = tr.Property
+    '''Height of the domain'''
     def _get_L_y(self):
         return self.d_y * (self.n_y-1)
 
     data_dir = tr.Property
+    '''Directory with the data'''
     def _get_data_dir(self):
         home_dir = expanduser('~')
         data_dir = join(home_dir, 'simdb', 'data', 'shear_zone', self.dir_name)
@@ -70,15 +72,16 @@ class DICGrid(bu.Model):
         n_t, n_e, n_a = u_tpa.shape # get the dimensions of the time and entry dimensions
         n_x, n_y = self.n_x, self.n_y
         if self.grid_column_first:
-            u_tjia = u_tpa.reshape(n_t, n_x, n_y, 2)  # for numbering from top right to bottom right
-            u_val = u_tjia
+            u_tija = u_tpa.reshape(n_t, n_x, n_y, 2)  # for numbering from top right to bottom right
+            u_val = u_tija
             #u_tjia = u_tpa.reshape(n_t, n_y, n_x, 2) # for numbering from bottom right to left
         else:
             u_tjia = u_tpa.reshape(n_t, n_y, n_x, 2) # for numbering from bottom right to left
             u_tija = np.einsum('tjia->tija', u_tjia)
-            u_val = u_tija
-        #u_tjia = u_tpa.reshape(n_t, n_x, n_y, 2) # for numbering from top right to bottom right
-        return  u_val #u_tjia #u_tjia #
+        if self.grid_number_vertical:
+            return u_tija[:,:,::-1,:]
+        else:
+            return u_tija
 
     n_t = tr.Property(depends_on='state_changed')
     '''Read the displacement data from the individual csv files'''
@@ -100,10 +103,10 @@ class DICGrid(bu.Model):
         #x_range = np.arange(n_x)[self.grid_x_slice] * self.d_x
         x_range = np.arange(n_x)[::-1] * self.d_x
         #y_range = np.arange(n_y) * self.d_y #for beams having grid number from bottom right to left
-        if self.grid_number_vertical:
-            y_range = np.arange(n_y)[::-1] * self.d_y #for beams having grid number from right top to bottom
-        else:
-            y_range = np.arange(n_y) * self.d_y #for beams having grid number from bottom right to left
+        # if self.grid_number_vertical:
+        #     y_range = np.arange(n_y)[::-1] * self.d_y #for beams having grid number from right top to bottom
+        # else:
+        y_range = np.arange(n_y) * self.d_y #for beams having grid number from bottom right to left
         #y_range = np.arange(n_y)[self.grid_y_slice] * self.d_y
         y_ij, x_ij = np.meshgrid(y_range, x_range)
         X_aij = np.array([x_ij, y_ij])
@@ -115,7 +118,7 @@ class DICGrid(bu.Model):
     '''
     @tr.cached_property
     def _get_U_ija(self):
-        return self.u_tija[self.end_t] - self.u_tija[0]
+        return self.u_tija[self.end_t] - self.u_tija[self.start_t]
 
     def update_plot(self, axes):
         ax = axes